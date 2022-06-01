3d726e1245cc2af03bf7588c1b9a54b67d42a86e
nachelysanchez <nashely-5@hotmail.com>
2022-06-01 02:16:50 -0400

    Agregando Spinner, y iniciando navigation

--- app/src/main/java/com/example/peopleregistercompose/MainActivity.kt
+++ app/src/main/java/com/example/peopleregistercompose/MainActivity.kt
@@ -3,22 +3,26 @@ package com.example.peopleregistercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.peopleregistercompose.ui.theme.PeopleRegisterComposeTheme
 
 
@@ -43,14 +47,91 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    RegistroPersonaScreen()
+    val navHostController = rememberNavController()
+
+    NavHost(navController = navHostController, startDestination = "ConsultaOcupaciones"){
+        composable("ConsultaOcupaciones"){
+            ConsultaOcupacionesScreen(navHostController = navHostController)
+        }
+        composable("RegistroOcupaciones"){
+            RegistroOcupacionesScreen(navHostController = navHostController)
+        }
+        composable("ConsultaPersonas"){
+            ConsultaPersonaScreen()
+        }
+        composable("RegistroPersona"){
+            RegistroPersonaScreen()
+        }
+    }
+}
+
+@Composable
+fun ConsultaOcupacionesScreen(navHostController: NavHostController){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Consulta de Ocupaciones")}
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                navHostController.navigate("RegistroOcupaciones")
+            }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+
+            val lista = listOf("Maestro", "Ingeniero", "Doctor", "Carpinterio")
+
+            LazyColumn(modifier = Modifier.fillMaxWidth()){
+                items(lista){
+                        ocupacion -> RowOcupacion(ocupacion = ocupacion)
+                }
+            }
+
+
+        }
+    }
+
+}
+
+@Composable
+fun RegistroOcupacionesScreen(navHostController: NavHostController){
+    var ocupacion by rememberSaveable(){
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = ocupacion,
+            onValueChange = {ocupacion = it},
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Nombre de la Ocupaci~n")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null)
+            }
+        )
+
+
+
+        OutlinedButton(onClick = { navHostController.navigateUp() }) {
+            Text(text = "Guardar")
+        }
+    }
 }
 
 @Composable
 fun ConsultaPersonaScreen(){
     Column(modifier = Modifier.padding(8.dp)) {
         Button(onClick = { /*TODO*/ }) {
-            Text(text = "Nuevo")
+            Text(text = "Nueva Ocupación")
         }
         val lista = listOf("Kelvin", "Alvaro", "Nachely", "Vismar", "Enel", "Junior")
 
@@ -64,11 +145,15 @@ fun ConsultaPersonaScreen(){
 
 @Composable
 fun RegistroPersonaScreen(){
+    var person by rememberSaveable(){
+        mutableStateOf("")
+    }
+
     val ocup = listOf("Maestro", "Ingeniero", "Doctor", "Carpinterio")
     Column(modifier = Modifier.padding(8.dp)) {
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = person,
+            onValueChange = {person = it},
             modifier = Modifier.fillMaxWidth(),
             label = {
                 Text(text = "Nombre de la Persona")
@@ -101,7 +186,7 @@ fun RegistroPersonaScreen(){
             onValueChange = {},
             modifier = Modifier.fillMaxWidth(),
             label = {
-                Text(text = "Personas")
+                Text(text = "Salario")
             },
             leadingIcon = {
                 Icon(
@@ -109,6 +194,10 @@ fun RegistroPersonaScreen(){
                     contentDescription = null)
             }
         )
+
+        OutlinedButton(onClick = { /*TODO*/ }) {
+            Text(text = "Guardar")
+        }
     }
 }
 
@@ -123,7 +212,7 @@ fun OcupacionesSpinner(ocupacion:List<String>){
     }
     Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
         Row(Modifier
-            .padding(24.dp)
+            .fillMaxWidth()
             .clickable {
                 expended = !expended
             }
@@ -131,6 +220,7 @@ fun OcupacionesSpinner(ocupacion:List<String>){
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ){
+
             Text(text = ocupacionText, fontSize = 18.sp, modifier = Modifier.padding(end = 8.dp))
             Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "")
             DropdownMenu(expanded = expended, onDismissRequest = { expended = false}) {
@@ -154,6 +244,13 @@ fun OcupacionesSpinner(ocupacion:List<String>){
 fun RowPersona(nombre:String){
     Row() {
         Text(text = "El nombre es: $nombre")
+    }
+}
+
+@Composable
+fun RowOcupacion(ocupacion:String){
+    Row() {
+        Text(text = "El nombre de la Ocupación es: $ocupacion")
     }
 }
 
